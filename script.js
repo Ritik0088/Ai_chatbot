a72dcaa5e14e85c6ebe46ad92fe00d6b0521db98
Ritik0088 <ritikkumar54128@gmail.com>
2026-06-06 18:54:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 import { apikey } from "./config.js";
+
+const apikey = "test";
+console.log(apikey);
 const chatbox = document.getElementById("chat-box")
 const userInput = document.getElementById("user-Input")
 const sendBtn = document.getElementById("send-btn");
